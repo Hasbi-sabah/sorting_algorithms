68b0e404f3c5053bfae858a3d3b764471a5d6759
H. Sabah <sabahhasbi00@gmail.com>
2023-07-21 07:03:44 -0700

    merge

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -32,5 +33,6 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
