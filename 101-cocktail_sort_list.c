f2a134221b29535d32f91928af4dfca75ee086ad
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-07-22 05:01:59 +0300

    task 5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -13,16 +13,16 @@ int swap_list(listint_t *n1, listint_t *n2, listint_t **list)
 
 	if (n1->n > n2->n)
 	{
-		n1->next = n2->next;
-		if (n2->next != NULL)
-			n2->next->prev = n1;
-		n2->next = n1;
-		n2->prev = n1->prev;
-		n1->prev = n2;
-		if (n2->prev != NULL)
-			n2->prev->next = n2;
+		if (n1->prev != NULL)
+			n1->prev->next = n2;
 		else
 			*list = n2;
+		if (n2->next != NULL)
+			n2->next->prev = n1;
+		n1->next = n2->next;
+		n2->prev = n1->prev;
+		n1->prev = n2;
+		n2->next = n1;
 		print_list(*list);
 		swapped = 1;
 	}
